100cbd42b838079cf88fb79859b9bb1ba088f027
ThiagoRaposo <thiago@ciclix.com>
2021-11-17 02:29:05 -0300

    atualizado gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import "dotenv/config";
 
 const app = express();
@@ -7,7 +7,7 @@ app.use(express.json());
 const port = process.env.PORT;
 
 app.listen(port, () => {
-  console.log("Servidor inicializado");
+  console.log(`Servidor inicializado e rodando na PORT: ${port}`);
 });
 
 let userId: number = 3;
